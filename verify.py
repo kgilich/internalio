da3c9f71dedc658da89f516c7b90f90b5ba9794c
Kryštof Gilich <krystof.gilich@firma.seznam.cz>
2021-03-25 17:17:53 +0100

    new roles verification

--- verify.py
+++ verify.py
@@ -1,5 +1,22 @@
+import sqlite3 as sql
+import datetime
+import os
+
 def verify_mail(email):
     if not "@decathlon.com" in email:
         return "chyba"
     else:
         return "ok"
+
+
+def permissions(user,aktualni_adresar):
+    con = sql.connect(os.path.join(aktualni_adresar, 'main.db'))
+    con.row_factory = sql.Row
+    cur = con.cursor()
+    cur.execute('SELECT * FROM uzivatele WHERE uzivatel = ?;', [user])
+    rows = cur.fetchall()
+    for row in rows:
+        if row['role'] == "SUPERUSER":
+            return "SUPERUSER"
+        else:
+            return "nečum"
